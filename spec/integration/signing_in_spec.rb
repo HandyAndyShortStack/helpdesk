a591d78266fb15259a88fd31181316331e961a95
Andy Short <andynshort@gmail.com>
2013-06-05 16:33:19 -0700

    implemented signing in via form

--- spec/integration/signing_in_spec.rb
+++ spec/integration/signing_in_spec.rb
@@ -16,4 +16,18 @@ feature "signing in" do
   
   end
 
+  scenario "signing in via form" do
+
+    User.find_by_email("helpdesk@example.com").confirm!
+
+    visit "/"
+    click_link "Sign in"
+    fill_in "Email", with: "helpdesk@example.com"
+    fill_in "Password", with: "password"
+    click_button "Sign in"
+
+    page.should have_content("Signed in successfully.")
+
+  end
+
 end
